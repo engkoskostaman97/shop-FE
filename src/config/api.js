39d500181642c0f1a6aef345c0c689d3396804ab
engkos <engkoskostaman759@gmail.com>
2022-10-23 00:10:14 +0700

    dev

--- src/config/api.js
+++ src/config/api.js
@@ -1,16 +1,16 @@
-export const API = () => {
-  const baseUrl = process.env.REACT_APP_BASEURL;
+import axios from "axios";
 
-  const executeAPI = async (endpoint, config) => {
-    const response = await fetch(baseUrl + endpoint, config);
-    const data = await response.json();
-    return data;
-  };
+// Create base URL API
+export const API = axios.create({
+  baseURL: process.env.REACT_APP_BASEURL,
+});
 
-  return {
-    get: executeAPI,
-    post: executeAPI,
-    patch: executeAPI,
-    delete: executeAPI,
-  };
+
+// Set Authorization Token Header
+export const setAuthToken = (token) => {
+  if (token) {
+    API.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  } else {
+    delete API.defaults.headers.commin["Authorization"];
+  }
 };
