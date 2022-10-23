eccf7285a3a5fdfd3570d7d88380374077c2f202
engkos <engkoskostaman759@gmail.com>
2022-10-23 11:21:25 +0700

    dev

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-// Init QueryClient and QueryClientProvider
-import { QueryClient, QueryClientProvider } from "react-query";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import { UserContextProvider } from "./context/userContext";
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { UserContextProvider } from './context/userContext';
+
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 // favicon
-import Favicon from "./assets/DumbMerch.png";
-const favicon = document.getElementById("idFavicon");
-favicon.setAttribute("href", Favicon);
+import Favicon from './assets/DumbMerch.png';
+const favicon = document.getElementById('idFavicon');
+favicon.setAttribute('href', Favicon);
 
-// Init Client
 const client = new QueryClient();
 
 ReactDOM.render(
@@ -28,7 +28,7 @@ ReactDOM.render(
       </QueryClientProvider>
     </UserContextProvider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
